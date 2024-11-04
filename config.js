e550e7b86eb040b777c224a00dd31de37f012102
Ashan1476 <smihira671@gmail.com>
2024-11-04 21:16:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? '': process.env.GITHUB_USERNAME,
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=auozRIAI#2hm3pAI_sDE4ZcPvFvALZqYarNMqSCiXaVL5J_Ysf2U",
+GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? 'RASH': process.env.GITHUB_USERNAME,
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '': process.env.GITHUB_AUTH_TOKEN,
 MONGODB: process.env.MONGODB || "mongodb+srv://myrender971:iwS8N2qB3Q8wMw83@cluster0.he9fd.mongodb.net/"    
 };
